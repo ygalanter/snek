ed0ddea6854a6496bfeaeb206db14731ca62d5c5
Yuriy Galanter <yuriy.galanter@wegalvanize.com>
2021-05-02 15:40:21 -0400

    Added comments

--- app/index.ts
+++ app/index.ts
@@ -3,16 +3,22 @@ import { me as device } from "device";
 import { field } from "./field"
 import { unlinkSync, writeFileSync } from "fs";
 
-const SEGMENT_SIZE = 15;
-const SCREEN_SIZE = { width: Math.floor(device.screen.width / SEGMENT_SIZE) * SEGMENT_SIZE, height: Math.floor(device.screen.height / SEGMENT_SIZE) * SEGMENT_SIZE };
-const INTERVAL = 150;
-const INITIAL_SNAKE_LENGTH = 5;
+const SEGMENT_SIZE = 15; // size of individual snake segment
 
+// screen size needs to be adjusted to be a multiple of segment size for coordinates (of apples etc. to matcg)
+const SCREEN_SIZE = { width: Math.floor(device.screen.width / SEGMENT_SIZE) * SEGMENT_SIZE, height: Math.floor(device.screen.height / SEGMENT_SIZE) * SEGMENT_SIZE };
+
+const INTERVAL = 150; // interval between snake movements for setInterval()
+const INITIAL_SNAKE_LENGTH = 5; // initial snake lengths in segments
+
+// direction for snake head to move
 let nextMoveX = 1;
 let nextMoveY = 0;
 
+// array to hold snake segments
 const segments: RectElement[] = [];
 
+/** Initializes title screen with PLAY and OPTIONS selection */
 function initTitleScreen() {
     document.getElementById('play').addEventListener('click', () => {
         initSnake();
@@ -20,11 +26,13 @@ function initTitleScreen() {
 }
 
 let prevEvent = 'right';
+/** Initializes Up Down left Right controls */
 function initControls() {
 
     document.getElementsByClassName('tap').forEach(control => {
         control.addEventListener('mousedown', (evt) => {
-
+            
+            // Preventing accidental move in the direction opposite current that would cause snake to collide with itself
             if (
                 (prevEvent === 'right' && control.id == 'left') ||
                 (prevEvent === 'left' && control.id == 'right') ||
@@ -60,11 +68,13 @@ function initControls() {
     })
 }
 
+/** Creates initial snake */
 function initSnake() {
 
     let rects = '';
     let i: number;
 
+    // Creating a string of SVG rectangles to represent snake
     for (let i = 0; i < INITIAL_SNAKE_LENGTH; i++) {
         rects += `<rect x="${SEGMENT_SIZE * i + SEGMENT_SIZE}" y="${SEGMENT_SIZE * 2}" class="${i < INITIAL_SNAKE_LENGTH - 1? 's' : 's h'}" />`
     }
@@ -72,18 +82,25 @@ function initSnake() {
     growSnake(rects);
 }
 
+/** Kicks of interval to start snake movement */
 function startSnake() {
     return setInterval(() => {
 
-        const oldHead = segments[segments.length - 1];
-        const newHead = segments.shift();
+        /*
+        We take first SVG from the snake array (its "tail") and move it to the end
+        with coordinates representing next snake move - it becomes new snake head
+        */
 
-        oldHead.class = 's';
-        newHead.class = 's h';
+        const oldHead = segments[segments.length - 1];
+        const newHead = segments.shift(); // former tail becomes new head
+
+        oldHead.class = 's'; // old head is no longer head, so we give it regular segment colors
+        newHead.class = 's h'; // new head gets head colors
 
         const newX = oldHead.x + SEGMENT_SIZE * nextMoveX;
         const newY = oldHead.y + SEGMENT_SIZE * nextMoveY;
 
+        // Verifying if new head coordinates collided with any part of snake body
         segments.forEach((segment: RectElement) => {
             if (newX === segment.x && newY === segment.y) {
                 console.log('BOOM!');
@@ -91,6 +108,7 @@ function startSnake() {
             }
         })
 
+        // if snake went past screen edge - it will emerge from opposite side
         if (newX >= SCREEN_SIZE.width) newHead.x = 0
         else if (newX <= 0) newHead.x = SCREEN_SIZE.width
         else newHead.x = newX;
@@ -101,6 +119,7 @@ function startSnake() {
 
         segments.push(newHead);
 
+        // if head ate apple - snake grows
         const apple = document.getElementById("apple") as RectElement;
         if (newHead.x === apple.x && newHead.y === apple.y) {
             growSnake();
@@ -112,30 +131,45 @@ function startSnake() {
 
 
 let viewId: number;
+
+/** Recreates SVG field with updates snake
+ * @param string Optional set of string representation of SVG rects to put into the field
+ */
 function growSnake(rects?: string) {
     console.log("***** GROWING!")
+
+    // stopping current snake movement
     clearInterval(iv);
 
+    // If parameter with SVG rects isn't passed - we recreate it from snake array
     if (!rects) {
+
+        // Snake needs to grow, so we add an extra rect
         rects = '<rect class="s" />';
 
+        // creating string of SVG rectangles from existing snake array
         for (let segment of segments) {
             rects += `<rect x="${segment.x}" y="${segment.y}" class="${segment.class}" />`
         }
     
+        // if previous SVG file exists - delete it
         if (viewId) {
             unlinkSync(`field${viewId}.view`);
         }
     }
 
+    // Creating new SVG field and writing it into file
     viewId = Math.random();
     const viewName = `field${viewId}.view`;
     writeFileSync(viewName, field.replace('<snake/>', rects), 'utf-8');
+
+    // loading newly created SVG view
     document.location.replace(`/private/data/${viewName}`).then(() => {
 
         segments.length = 0;
         const rects = document.getElementsByClassName("s");
 
+        // recreating snake array from SVG elements
         rects.forEach((segment: RectElement) => {
             segments.push(segment)
         });
@@ -147,6 +181,7 @@ function growSnake(rects?: string) {
     })
 }
 
+/** Puts apple in random location on the screen */
 function setApple() {
     const apple = document.getElementById("apple") as RectElement;
     apple.x = Math.floor(Math.random() * (SCREEN_SIZE.width - SEGMENT_SIZE * 2) / SEGMENT_SIZE) * SEGMENT_SIZE + SEGMENT_SIZE * 2;
